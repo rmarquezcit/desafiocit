3680a0602c80bfd5c0c95eee745636ecb5a902e3
fernandosantana <l.fernandosantana@gmail.com>
2016-10-19 21:49:50 -0200

    remoção do teste

--- JogoVida/src/Main.java
+++ JogoVida/src/Main.java
@@ -3,9 +3,5 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Jogo jogo = new Jogo();
-		
-		String[][] tabuleiro = jogo.inicializaTabuleiroTeste();
-		jogo.resultado(tabuleiro);
 	}
 }
