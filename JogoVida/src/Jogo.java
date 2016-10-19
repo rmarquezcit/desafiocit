43d712ed31e3cf58690087e0db1d23243411e88f
Victor César Dantas Gouveia <vcdg@cesar-ldap.intranet>
2016-10-19 20:48:08 -0300

    adicionando inicializacao do tabuleiro

--- JogoVida/src/Jogo.java
+++ JogoVida/src/Jogo.java
@@ -1,5 +1,63 @@
 
 public class Jogo {
+
+	private int x, y;
+	private String tabuleiro[][];
+	
+	public Jogo(int x, int y){
+		this.x = x;
+		this.y = y;
+		this.tabuleiro = new String[x][y];
+		start();
+	}
+	
+	public void start(){
+		for(int i = 0; i < x; i++){
+			for(int j = 0; j < y; j++){
+				if(((i >= 1) && (i < x-1)) && ((j >= 1) && (j < y -1))){
+					tabuleiro[i][j] = "X";
+				}else{
+					tabuleiro[i][j] = "0";
+				}
+				if((i == 1) && (j == 1)){
+					tabuleiro[i][j] = "0";
+				}
+
+			}
+		}
+		
+		continuar();
+	}
+	
+	private void continuar(){
+		for(int i = 0; i < x; i++){
+			for(int j = 0; j < y; j++){
+				if(tabuleiro[i][j].equals("X")){
+					//verificar numero de vizinhos e aplicar logica vivo
+				}else{
+					//verificar numero de vizinhos e aplicar logica morto
+				}
+
+			}
+		}
+	}
+	
+	
+	
+	public void print(){
+		for(int i = 0; i < x; i++){
+			for(int j = 0; j < y; j++){
+				System.out.print(tabuleiro[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Jogo j = new Jogo(4,5);
+		j.print();
+	}
+	
 	
 	public String logicaMorto(int vizinhos){
 		String str = "0";
